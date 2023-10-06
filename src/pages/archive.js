1466289de97cf1edfe3418dfda809da4a16fb53d
NekoShaaark <nekoshaaark@gmail.com>
2023-10-06 21:23:56 +0200

    Updated Archive page and added favicon

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Archive.module.css'
 
 
@@ -11,6 +12,7 @@ export default function Archive() {
 
       <h1 className={styles.title}>Archive</h1>
       <div className={styles.text}>&quot;How did you get here?&quot;</div>
+      <Image src="/dedNiko.png" width={326} height={112} alt="dedNiko"/>
     </>
   )
 }
